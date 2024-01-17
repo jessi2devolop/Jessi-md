dc71fe9b273df4304ec5338bdf38f92b2cf35b61
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 14:48:44 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -16,7 +16,7 @@ handler.tags = ['downloader']
 handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
 
 export default handler
-*/
+
 
 import fetch from 'node-fetch';
 
@@ -33,3 +33,33 @@ handler.tags = ['downloader']
 handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
 
 export default handler
+*/
+
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
+    await m.reply(`Ｌｏａｄｉｎｇ．．．`);
+    
+    try {
+        let response = await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`);
+        let data = await response.json();
+        
+        if (data.status) {
+            await conn.sendMessage(m.chat, { video: { url: data.data[0].download_link } }, m);
+        } else {
+            throw new Error('Error in response data');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        m.reply('Failed to fetch Instagram data. Please try again later.');
+    }
+}
+
+handler.help = ['ig'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
+
+export default handler;
+

502a6bcc6cb196cc5afa75d27e55d06be4625f51
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 22:39:07 +0530

    Create downloader-aptoide.js

---
+++ plugins/downloader-aptoide.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import { search, download } from 'aptoide-scraper';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example: ${usedPrefix + command} com.example.app`;
+    
+    try {
+        let results = await search(args[0]);
+        
+        if (results.length === 0) {
+            return m.reply(`No results found for '${args[0]}'.`);
+        }
+
+        let appInfo = results[0];
+        let appData = await download(appInfo.package);
+
+        let inf = `*App Name:* ${appData.name}\n`;
+        inf += `*App Package ID:* ${appData.package}\n`;
+        inf += `*Last Update:* ${appData.lastup}\n`;
+        inf += `*App Size:* ${appData.size}\n\n`;
+
+        const filePath = `./${appData.name}.apk`;
+
+        if (appData.size > 150) {
+            return m.reply(`File size is larger than 150 MB. Unable to send.`);
+        }
+
+        let response = await fetch(appData.dllink);
+        let buffer = await response.buffer();
+
+        await conn.sendDocument(m.chat, buffer, m, { mimetype: 'application/vnd.android.package-archive', filename: `${appData.name}.apk`, caption: inf });
+    } catch (error) {
+        console.error(error);
+        return m.reply('Error while fetching APK information. Please try again later.');
+    }
+};
+
+handler.help = ['aptodl <package-id>'];
+handler.tags = ['downloader'];
+handler.command = /^(aptodl)$/i;
+
+export default handler;

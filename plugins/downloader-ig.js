0810ee0ec2434c3acfbcda7e0fb1454007500f88
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 14:10:43 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+/*import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
@@ -7,6 +7,24 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
             const json = await tes.json();
             const igdl = json.result;
             await conn.sendFile(m.chat, igdl, 'error.mp4', '❤️ done here', m);
+}
+
+handler.help = ['ig'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(ig(dl)?|insta|instagram(dl)?)$/i
+
+export default handler
+*/
+
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
+    await m.reply(`Ｌｏａｄｉｎｇ．．．`);
+    let data;
+ try{ data= await (await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`)).json();}
+ return conn.sendMessage(m.chat, {video : {url : data.result[0] },caption: cap, }, m);
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')

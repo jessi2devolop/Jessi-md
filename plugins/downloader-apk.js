a74ad45ccebf371c02aed5d9ffa1e253258299c2
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 15:04:50 +0530

    Update downloader-apk.js

--- plugins/downloader-apk.js
+++ plugins/downloader-apk.js
@@ -1,11 +1,12 @@
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if (!args[0]) throw `Ex: ${usedPrefix + command} https://play.google.com/store/apps/details?id=com.linecorp.LGGRTHN`;
-	let res = await apkDl(args[0]);
-	await m.reply('_In progress, please wait..._');
-	conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m });
+    if (!args[0]) throw `Ex: ${usedPrefix + command} https://play.google.com/store/apps/details?id=com.linecorp.LGGRTHN`;
+    let res = await apkDl(args[0]);
+    await m.reply('_In progress, please wait..._');
+    conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m });
 };
 
 handler.help = ['apkdl'];
@@ -15,32 +16,39 @@ handler.command = /^(apkdl)$/i;
 export default handler;
 
 async function apkDl(url) {
-	const formData = new FormData();
-	formData.append('x', 'downapk');
-	formData.append('t', 1);
-	formData.append('google_id', url);
-	formData.append('device_id', '');
-	formData.append('language', 'en-US');
-	formData.append('dpi', 480);
-	formData.append('gl', 'SUQ=');
-	formData.append('model', '');
-	formData.append('hl', 'en');
-	formData.append('de_av', '');
-	formData.append('aav', '');
-	formData.append('android_ver', 5.1);
-	formData.append('tbi', 'arm64-v8a');
-	formData.append('country', 0);
+    const form = new FormData();
+    form.append('x', 'downapk');
+    form.append('t', 1);
+    form.append('google_id', url);
+    form.append('device_id', '');
+    form.append('language', 'en-US');
+    form.append('dpi', 480);
+    form.append('gl', 'SUQ=');
+    form.append('model', '');
+    form.append('hl', 'en');
+    form.append('de_av', '');
+    form.append('aav', '');
+    form.append('android_ver', 5.1);
+    form.append('tbi', 'arm64-v8a');
+    form.append('country', 0);
 
-	let res = await fetch('https://apk.support/gapi/index.php', {
-		method: 'post',
-		body: formData,
-	});
+    try {
+        let res = await fetch('https://apk.support/gapi/index.php', {
+            method: 'POST',
+            body: form,
+            headers: form.getHeaders(),
+        });
 
-	let $ = cheerio.load(await res.text());
-	let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim().split(' ')[0];
-	let download = $('div.browser > div.dvContents > ul > li > a').attr('href');
-	if (!download) throw 'Can\'t download the apk!';
-	let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
-	return { fileName, mimetype, download };
+        let $ = cheerio.load(await res.text());
+        let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim();
+        let download = $('div.browser > div.dvContents > ul > li > a').attr('href');
+        
+        if (!download) throw 'Can\'t find the APK download link!';
+        
+        let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type');
+        return { fileName, mimetype, download };
+    } catch (error) {
+        console.error('Error:', error.message);
+        throw 'Failed to fetch APK information. Please try again later.';
+    }
 }
-

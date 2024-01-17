430485c1d2cb1368853f7ec6805d2ad69e606f86
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 11:37:44 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
     await m.reply(`Ｌｏａｄｉｎｇ．．．`);
-    let tes = await fetch(`https://inrl-web.onrender.com/api/insta?url=${args[0]}`);
+    let tes = await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`);
             const json = await tes.json();
             const igdl = json.result;
             await conn.sendFile(m.chat, igdl, 'error.mp4', '❤️ done here', m);

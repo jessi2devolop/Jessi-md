6f07f5f1d89921ff5222e8d8e2b4c28470e838a8
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-17 14:22:51 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,4 +1,5 @@
-/*import fetch from 'node-fetch';
+/*
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
@@ -22,9 +23,8 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(args[0])) throw m.reply(`Use example ${usedPrefix}${command} link`);
     await m.reply(`Ｌｏａｄｉｎｇ．．．`);
-    let data;
- try{ data= await (await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`)).json();}
- return conn.sendMessage(m.chat, {video : {url : data.result[0] },caption: cap, }, m);
+    let data = await fetch(`https://vihangayt.me/download/instagram2?url=${args[0]}`).json();
+    await conn.sendMessage(m.chat, {video : {url : data.result[0] },caption: cap, }, m);
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')

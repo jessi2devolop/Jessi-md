49793da11503b6ce0f295dd09334c8cb5f1b5c0a
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-01-21 13:52:11 +0530

    Update ai-animediff.js

--- plugins/ai-animediff.js
+++ plugins/ai-animediff.js
@@ -1,25 +1,30 @@
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-         let wm = global.me
+    let wm = global.me;
 
-        if (!text) throw `This command generates image from texts\n\n Example usage\n${ usedPrefix + command } 1girl, blush, megane, school uniform`
-        await m.reply('*Processing image*')
-        await conn.relayMessage(m.chat, { reactionMessage: { key: m.key, text: '⌛'  }}, { messageId: m.key.id })
-        try {
-        let url = `https://api.xyroinee.xyz/api/ai/animediffusion?q=${text}&apikey=${global.xyro}`
-        await conn.sendFile(m.chat, await(await fetch(url)).buffer(), 'image.jpg', wm, m)
-        m.react(done)
-      } catch (e) {
-        console.log(e)
-        m.reply(eror)
-      }
+    if (!text) throw `This command generates an image from texts\n\n Example usage\n${usedPrefix + command} 1girl, blush, megane, school uniform`;
+    await m.reply('*Processing image*');
+    await conn.relayMessage(m.chat, { reactionMessage: { key: m.key, text: '🤓' } }, { messageId: m.key.id });
 
+    try {
+        const response = await fetch(`https://vihangayt.me/tools/photoleap?q=${text}`);
+        const data = await response.json();
+
+        if (!data.status) throw 'Failed to fetch image data from API';
+        if (!data.data) throw 'Image URL not found in the API response';
+
+        const url = data.data;
+        await conn.sendFile(m.chat, await (await fetch(url)).buffer(), 'image.jpg', wm, m);
+        m.react('🤳');
+    } catch (e) {
+        console.error(e);
+        m.reply('❌ Error generating image');
     }
+};
 
+handler.help = ['aiimg <text>'];
+handler.tags = ['ai'];
+handler.command = /^(aiimg)$/i;
 
-handler.help = ['animediff <text>']
-handler.tags = ['ai']
-handler.command = /^(animediff)$/i
-
-export default handler
+export default handler;

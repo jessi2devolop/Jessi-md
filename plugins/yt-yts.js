05cd4d5f9f29d5cefb936dd7aea7aaa7d5a232a4
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-02-29 12:28:58 +0530

    Update yt-yts.js

--- plugins/yt-yts.js
+++ plugins/yt-yts.js
@@ -1,24 +1,35 @@
-import yts from 'yt-search'
-import fs from 'fs'
+import yts from 'yt-search';
+import fs from 'fs';
 
-let handler = async (m, {conn, text }) => {
-  if (!text) throw 'What are you looking for?'
-  await conn.reply(m.chat, global.wait, m)
-  let results = await yts(text)
-  let tes = results.all
-  let teks = results.all.map(v => {
-    switch (v.type) {
-      case 'video': return `
-° *_${v.title}_*
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duration :_* ${v.timestamp}
-↳ 📥 *_Uploaded :_* ${v.ago}
-↳ 👁 *_Views :_* ${v.views}`}}).filter(v => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
-  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
-}
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'What are you looking for?';
+  //await conn.reply(m.chat, global.wait, m);
+  let results = await yts(text);
+  let videos = results.videos.splice(0, 20);
 
-handler.help = ['yts <query>']
-handler.tags = ['downloader']
-handler.command = /^yts(earch)?$/i
+  if (videos.length < 1) {
+    return await conn.sendReply('*No results found!*', m);
+  }
 
-export default handler
+  let list = `_*Search results for ${text}:*_\n\n`;
+  let index = 0;
+
+  for (let video of videos) {
+    const url = video.url;
+    const title = video.title;
+    const duration = video.duration.timestamp;
+
+    if (title && duration) {
+      index++;
+      list += `${index}. *_${title} (${duration})_* _( ${url} )_\n`;
+    }
+  }
+
+  conn.sendFile(m.chat, videos[0].thumbnail, 'yts.jpeg', list, m);
+};
+
+handler.help = ['yts <query>'];
+handler.tags = ['downloader'];
+handler.command = /^yts(earch)?$/i;
+
+export default handler;

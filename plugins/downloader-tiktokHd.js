86bf4ffc9e33649bc5c8c13397f61ccfc93fd549
jessi2devolop <108852569+jessi2devolop@users.noreply.github.com>
2024-04-18 14:18:33 +0530

    Update downloader-tiktokHd.js

--- plugins/downloader-tiktokHd.js
+++ plugins/downloader-tiktokHd.js
@@ -1,6 +1,5 @@
 // downloader-tiktokHd.js
 
-
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
@@ -9,21 +8,25 @@ let handler = async (m, { conn, args }) => {
     try {
         let tiktokUrl = args[0].trim();
 
+        
         if (!/(vm\.tiktok\.com|tiktok\.com)/i.test(tiktokUrl)) {
             throw 'Please enter a valid TikTok link.';
         }
+        
 
-        let apiUrl = `https://api-rest-jessi2devolop.koyeb.app/api/dowloader/tiktok?url=${encodeURIComponent(tiktokUrl)}`;
+        let apiUrl = `https://api-rest-jessi2devolop.koyeb.app/api/dowloader/tikok?url=${encodeURIComponent(tiktokUrl)}`;
 
         let response = await fetch(apiUrl);
         if (response.ok) {
             let data = await response.json();
 
-            if (data.status === true && data.result && data.result.video_HD && data.result.pp && data.result.author_name && data.result.description) {
+            if (data.status === true && data.result && data.result.video_HD) {
                 let videoBuffer = await fetch(data.result.video_HD).then(res => res.buffer());
-                let ppr = await shortUrl(data.result.pp);
-                let caption = `*Author:* [${data.result.author_name}]\n*Description:* ${data.result.description}\n*Profile Picture:* ${ppr}`;
 
+                
+                let caption = `*Author:* [${data.result.author_name}]\n*Description:* ${data.result.description}`;
+
+                
                 conn.sendFile(m.chat, videoBuffer, 'video.mp4', caption, m);
             } else {
                 conn.reply(m.chat, 'Failed to download TikTok video. Please try again later.', m);
@@ -42,18 +45,3 @@ handler.tags = ['downloader'];
 handler.command = /^(tiktokhd)$/i;
 
 export default handler;
-
-async function shortUrl(url) {
-    try {
-        let response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
-        if (response.ok) {
-            let shortenedUrl = await response.text();
-            return shortenedUrl;
-        } else {
-            throw 'Error shortening URL';
-        }
-    } catch (error) {
-        console.error('URL shortening error:', error);
-        throw 'An error occurred while shortening the URL';
-    }
-            } 
